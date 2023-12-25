e181a82f7f441b74fe79e92da31a2a982ecc4535
Metixes <metixes@gmail.com>
2023-12-25 20:48:20 +0200

    add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="main">
+    <button>button 1</button>
+    <button>button 2</button>
+    <select name="tests" id="test1-select">
+      <option value="">--Please choose an option--</option>
+      <option value="test1">test1</option>
+    </select>
+    <select name="tests" id="test2-select">
+      <option value="">--Please choose an option--</option>
+      <option value="test1">test1</option>
+    </select>
   </main>
 </template>
+
+<script setup></script>
+
+<style scoped>
+.main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
